9d4019285ba95fcbe39bcf43a3b03b0e1f9f5988
bubble <1065046544@qq.com>
2023-04-19 18:12:21 +0800

    区分开发和生产环境

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,2 +1,10 @@
-export const BASE_URL = "http://codercba.com:5173";
-export const TIME_OUT = 10000;
+let BASE_URL;
+const TIME_OUT = 10000;
+
+if (import.meta.env.PROD) {
+  BASE_URL = "http://codercba.com:5173";
+  // BASE_URL = "http://codercba.prod:5173";
+} else {
+  BASE_URL = "http://codercba.dev:5173";
+}
+export { BASE_URL, TIME_OUT };
