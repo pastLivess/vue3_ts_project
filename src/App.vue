c14cacad16c1e130fd2063319835d64a1a258d3c
bubble <1065046544@qq.com>
2023-04-19 18:48:32 +0800

    页面占据百分百宽高

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="App">
+  <div class="app">
     <h2>App</h2>
     <RouterLink to="/main">主要</RouterLink>
     <RouterLink to="/login">login</RouterLink>
@@ -9,4 +9,10 @@
 
 <script setup lang="ts"></script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.app {
+  width: 100vh;
+  height: 100vh;
+  background-color: skyblue;
+}
+</style>
