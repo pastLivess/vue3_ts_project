b7e32b0e59bf96e8d4c22e499221817b37347817
bubble <1065046544@qq.com>
2023-04-20 21:34:38 +0800

    main页面的aside初步搭建

---
+++ src/views/main/c-cpns/main-header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-header">
+    <img class="main-header-logo" src="@/assets/img/logo.svg" alt="" />
+    <h2 class="main-header-title">CMS-后台管理系统</h2>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less">
+.main-header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  height: 40px;
+  .main-header-logo {
+    width: 32px;
+    height: 32px;
+  }
+  .main-header-title {
+    font-size: 16px;
+  }
+}
+</style>
