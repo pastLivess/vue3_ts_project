e68a802f51b21affe8a1e7a9cc869c271081437a
bubble <1065046544@qq.com>
2023-04-21 10:21:00 +0800

    按钮切换menu菜单展示与否

---
+++ src/components/main-header/main-header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="main-header">
+    <div class="menu-icon" @click="showMenuClick">
+      <el-icon size="26px">
+        <component :is="!isShowMenu ? 'Expand' : 'Fold'"></component>
+      </el-icon>
+    </div>
+    <div class="content">
+      <div class="breadcrumb">面包屑</div>
+      <div class="user">个人信息</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const emits = defineEmits(["foldChange"]);
+const isShowMenu = ref(false);
+function showMenuClick() {
+  isShowMenu.value = !isShowMenu.value;
+  emits("foldChange", isShowMenu.value);
+}
+</script>
+
+<style scoped lang="less">
+.main-header {
+  display: flex;
+  align-items: center;
+  height: 60px;
+  .content {
+    display: flex;
+    flex: 1;
+    margin-left: 15px;
+    .breadcrumb {
+      flex: 1;
+    }
+  }
+}
+</style>
