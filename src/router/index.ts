074a52602b14c935cab713edfa8fd57d5951d15e
bubble <1065046544@qq.com>
2023-04-21 16:00:13 +0800

    动态路由-拆分每一个独立的路由到文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,18 +15,7 @@ const router = createRouter({
     {
       path: "/main",
       component: () => import("@/views/main/main.vue"),
-      children: [
-        {
-          path: "/main/analysis/dashboard",
-          component: () =>
-            import("@/views/main/c-cpns/analysis/dashboard/dashboard.vue")
-        },
-        {
-          path: "/main/analysis/overview",
-          component: () =>
-            import("@/views/main/c-cpns/analysis/overview/overview.vue")
-        }
-      ]
+      children: []
     },
     {
       path: "/:pathMatch(.*)",
@@ -34,6 +23,49 @@ const router = createRouter({
     }
   ]
 });
+const localRoutes = [
+  {
+    path: "/main/analysis/overview",
+    component: () => import("@/views/main/analysis/overview/overview.vue")
+  },
+  {
+    path: "/main/analysis/dashboard",
+    component: () => import("@/views/main/analysis/dashboard/dashboard.vue")
+  },
+  {
+    path: "/main/system/department",
+    component: () => import("@/views/main/system/department/department.vue")
+  },
+  {
+    path: "/main/system/menu",
+    component: () => import("@/views/main/system/menu/menu.vue")
+  },
+  {
+    path: "/main/system/role",
+    component: () => import("@/views/main/system/role/role.vue")
+  },
+  {
+    path: "/main/system/user",
+    component: () => import("@/views/main/system/user/user.vue")
+  },
+  {
+    path: "/main/product/category",
+    component: () => import("@/views/main/product/category/category.vue")
+  },
+  {
+    path: "/main/product/goods",
+    component: () => import("@/views/main/product/goods/goods.vue")
+  }
+];
+router.addRoute("main", localRoutes[0]);
+router.addRoute("main", localRoutes[1]);
+router.addRoute("main", localRoutes[2]);
+router.addRoute("main", localRoutes[3]);
+router.addRoute("main", localRoutes[4]);
+router.addRoute("main", localRoutes[5]);
+router.addRoute("main", localRoutes[6]);
+router.addRoute("main", localRoutes[7]);
+
 router.beforeEach((to, from, next) => {
   const token = localCache.getCache("token");
   if (!token && to.path === "/main") {
