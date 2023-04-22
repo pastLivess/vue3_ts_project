713897f12106b9806873857392012e7cad05126c
bubble <1065046544@qq.com>
2023-04-22 14:42:58 +0800

    表格数据动态展示-新增用户的模态框搭建

---
+++ src/views/main/system/user/c-cpns/user-modal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="user-modal">
+    <el-dialog center width="35%" title="新建用户" :model-value="true">
+      <el-form label-width="100px">
+        <el-form-item label="用户名">
+          <el-input autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="真实姓名">
+          <el-input autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="手机号码">
+          <el-input autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="选择角色">
+          <el-select style="width: 100%"></el-select>
+        </el-form-item>
+        <el-form-item label="选择部门">
+          <el-select style="width: 100%"></el-select>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button>取消</el-button>
+          <el-button type="primary"> 确定 </el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const modalValue = ref(false);
+</script>
+
+<style scoped lang="less">
+.user-modal {
+}
+</style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -2,12 +2,14 @@
   <div class="user">
     <user-search @resetForm="handlerResetForm" @queryForm="handlerQueryForm" />
     <user-content ref="useContentRef" />
+    <user-modal />
   </div>
 </template>
 
 <script setup lang="ts">
 import userSearch from "./c-cpns/user-search.vue";
 import userContent from "./c-cpns/user-content.vue";
+import userModal from "./c-cpns/user-modal.vue";
 import { ref } from "vue";
 const useContentRef = ref<InstanceType<typeof userContent>>();
 function handlerResetForm() {
