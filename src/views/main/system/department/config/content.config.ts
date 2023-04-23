b6e72eda1421a58b0d31918f98129d80da651152
bubble <1065046544@qq.com>
2023-04-23 11:36:17 +0800

    对content中的table进行抽取

---
+++ src/views/main/system/department/config/content.config.ts
@@ -0,0 +1,59 @@
+const contentConfig = {
+  header: {
+    title: "部门列表",
+    btnTitle: "新建部门"
+  },
+  propsList: [
+    {
+      type: "selection",
+      label: "选择",
+      width: "60px",
+      align: "center"
+    },
+    {
+      type: "index",
+      label: "序号",
+      width: "80px",
+      align: "center"
+    },
+    {
+      type: "normal",
+      label: "部门名称",
+      prop: "name",
+      width: "150px",
+      align: "center"
+    },
+    {
+      type: "normal",
+      label: "部门领导",
+      prop: "leader",
+      width: "150px",
+      align: "center"
+    },
+    {
+      type: "normal",
+      label: "上级部门",
+      prop: "parentId",
+      width: "150px",
+      align: "center"
+    },
+    {
+      type: "timer",
+      label: "创建时间",
+      prop: "createAt",
+      align: "center"
+    },
+    {
+      type: "timer",
+      label: "创建时间",
+      prop: "updateAt",
+      align: "center"
+    },
+    {
+      type: "handler",
+      label: "操作",
+      align: "center"
+    }
+  ]
+};
+export default contentConfig;
