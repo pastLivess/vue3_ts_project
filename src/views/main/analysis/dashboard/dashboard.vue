553661f19d560b61041f2cc176b030254b7e5d32
bubble <1065046544@qq.com>
2023-04-26 10:00:03 +0800

    商品统计页面头部数据的请求

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="dashboard">
     <el-row :gutter="10">
-      <template v-for="item in 4" :key="item">
+      <template v-for="item in amountList" :key="item.amount">
         <el-col :span="6">
-          <count-card />
+          <count-card
+            v-bind="item"
+            :count1="item.number1"
+            :count2="item.number2"
+          />
         </el-col>
       </template>
     </el-row>
@@ -11,7 +15,13 @@
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
 import countCard from "./c-cpns/count-card.vue";
+import useAnalysisStore from "@/store/module/analysis";
+const analysisStore = useAnalysisStore();
+analysisStore.fetchAnalysisDataAction();
+// 获取数据
+const { amountList } = storeToRefs(analysisStore);
 </script>
 
 <style scoped lang="less"></style>
