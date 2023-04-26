608f50bf6411ccdbbdf4d21cd577758ec1a913e1
bubble <1065046544@qq.com>
2023-04-26 09:49:24 +0800

    商品统计页面

---
+++ src/views/main/analysis/dashboard/c-cpns/count-card.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="count-card">
+    <div class="header">
+      <span>{{ title }}</span>
+      <el-tooltip :content="tips" placement="top" effect="light">
+        <el-icon><Warning /></el-icon>
+      </el-tooltip>
+    </div>
+    <div class="content">
+      <div class="count">{{ count1 }}</div>
+    </div>
+    <div class="footer">
+      <span>{{ subtitle }}</span>
+      <span>{{ count2 }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface IProps {
+  title?: string;
+  tips?: string;
+  subtitle?: string;
+  count1?: number;
+  count2?: number;
+}
+withDefaults(defineProps<IProps>(), {
+  title: "商品总数量",
+  tips: "所有商品总销售额",
+  subtitle: "商品总数量",
+  count1: 509989,
+  count2: 50989
+});
+</script>
+
+<style scoped lang="less">
+.count-card {
+  display: flex;
+  flex-direction: column;
+  padding: 0 20px;
+  height: 130px;
+  background: white;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+  .header {
+    display: flex;
+    height: 38px;
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.45);
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+
+  .content {
+    display: flex;
+    margin-left: 0px;
+    font-size: 26px;
+    color: rgba(0, 0, 0, 0.85);
+    flex: 1;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+  }
+
+  .footer {
+    display: flex;
+    height: 38px;
+    line-height: 38px;
+    font-size: 14px;
+    letter-spacing: 1px;
+    color: rgba(0, 0, 0, 0.85);
+    border-top: 1px solid #f0f0f0;
+  }
+}
+</style>
--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,25 +1,17 @@
 <template>
-  <div>
-    <h2 class="title">{{ counter }}</h2>
-    <button @click="increment">+</button>
+  <div class="dashboard">
+    <el-row :gutter="10">
+      <template v-for="item in 4" :key="item">
+        <el-col :span="6">
+          <count-card />
+        </el-col>
+      </template>
+    </el-row>
   </div>
 </template>
 
 <script setup lang="ts">
-import { nextTick } from "vue";
-import { ref } from "vue";
-
-const counter = ref(0);
-function increment() {
-  counter.value++;
-  const title = document.querySelector(".title");
-  // 每次获取的值都是上一次的值 与react中的setState一样
-  // 数据是异步更新的,拿取不到最后的值
-  console.log(title?.textContent);
-  nextTick(() => {
-    console.log(title?.textContent);
-  });
-}
+import countCard from "./c-cpns/count-card.vue";
 </script>
 
 <style scoped lang="less"></style>
